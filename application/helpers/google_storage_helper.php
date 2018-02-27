e67979cd14e6ce68a49bf9da17023b82673fb504
Matteo Ceradini <matteo.ceradini@gmail.com>
2018-02-27 11:29:01 +0100

    up

--- application/helpers/google_storage_helper.php
+++ application/helpers/google_storage_helper.php
@@ -1,23 +1,21 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 # Includes the autoloader for libraries installed with composer
-require '/vendor/autoload.php';
+require  FCPATH . '/vendor/autoload.php';
 
 # Imports the Google Cloud client library
 use Google\Cloud\Storage\StorageClient;
 
 function upload_file($file_path = false)
 {
-	if($file_path){
-		echo $file_path; die();
-		
+	if($file_path){		
 		# Your Google Cloud Platform project ID
 		$projectId = 'pacific-apex-195814';
 
 		# Instantiates a client
 		$storage = new StorageClient([
 			'projectId'   => $projectId,
-			'keyFilePath' => __DIR__ . '/keys/AJarvis-5bfebda57c5c.json'
+			'keyFilePath' =>  FCPATH . '/keys/AJarvis-5bfebda57c5c.json'
 		]);
 
 		echo 'ok';
