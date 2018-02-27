627abcb6c72d78634ac1ca4e6c7667162d0f0b8c
Matteo Ceradini <matteo.ceradini@gmail.com>
2018-02-27 11:17:14 +0100

    up

--- application/helpers/google_storage_helper.php
+++ application/helpers/google_storage_helper.php
@@ -1,15 +1,16 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+# Includes the autoloader for libraries installed with composer
+require __DIR__ . '/vendor/autoload.php';
+
+# Imports the Google Cloud client library
+use Google\Cloud\Storage\StorageClient;
+
 function upload_file($file_path = false)
 {
 	if($file_path){
 		echo $file_path; die();
-		# Includes the autoloader for libraries installed with composer
-		require __DIR__ . '/vendor/autoload.php';
-
-		# Imports the Google Cloud client library
-		use Google\Cloud\Storage\StorageClient;
-
+		
 		# Your Google Cloud Platform project ID
 		$projectId = 'pacific-apex-195814';
 
