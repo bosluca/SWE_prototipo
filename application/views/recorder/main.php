892e3c3e5b85448bfeb81675a2b58818e1ad4664
Matteo Ceradini <matteo.ceradini@gmail.com>
2018-03-01 09:54:44 +0100

    up

--- application/views/recorder/main.php
+++ application/views/recorder/main.php
@@ -16,7 +16,7 @@
 </div>
 <div class="row justify-content-center mt-3">
 	<div class="col-sm-12 col-lg-6">
-		<div class="progress d-none">
+		<div class="progress progress-bar-striped progress-bar-animated d-none">
 	  		<div class="progress-bar" id="uploadProgress" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
 		</div>
 	</div>
