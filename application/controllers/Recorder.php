427a07633fc7c84a5f2f168d268c8645df2f2aca
Matteo Ceradini <matteo.ceradini@gmail.com>
2018-03-02 09:57:39 +0100

    debug

--- application/controllers/Recorder.php
+++ application/controllers/Recorder.php
@@ -38,7 +38,7 @@ class Recorder extends CI_Controller {
 					// delete old file saved on server
 					unlink($wav_file);
 
-					echo transcribe_async_gcs($fname . '.FLAC');
+					echo 'ciao';// transcribe_async_gcs($fname . '.FLAC');
 				}
 				else {
 					echo 'unable to upload file on google-storage';
