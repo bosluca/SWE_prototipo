13d53d8ba6d7b4a2ff2af0de5a8331c12a9b5a74
Matteo Ceradini <matteo.ceradini@gmail.com>
2018-03-02 17:15:16 +0100

    up

--- application/helpers/google_speech_helper.php
+++ application/helpers/google_speech_helper.php
@@ -21,7 +21,7 @@ use Google\Cloud\Core\ExponentialBackoff;
  *
  * @return string the text transcription
  */
-function transcribe_async_gcs($objectName, $languageCode = 'it-IT', $options = ['encoding' => 'WAV', 'sampleRateHertz' => 48000])
+function transcribe_async_gcs($objectName, $languageCode = 'it-IT', $options = ['encoding' => 'wav', 'sampleRateHertz' => 48000])
 {
     $CI = & get_instance();
     $CI->config->load('google_cloud');
