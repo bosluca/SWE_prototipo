c18353718e593be4a4ba3e244f68f2a79212454b
Matteo Ceradini <matteo.ceradini@gmail.com>
2018-03-05 18:37:11 +0100

    up

--- application/views/analyzer/main.php
+++ application/views/analyzer/main.php
@@ -1,21 +1,21 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 <div class="row">
-	<?php if(count($sentences_positive) > 0) : ?>
-		<p class="h1">Frasi positive:</p>
+	<?php if(count($sentences_positive) > 0 || true) : ?>
 		<div class="col-sm col-md">
+			<p class="h3">Frasi positive:</p>
 			<?php echo $this->load->view('analyzer/sentencesTable', array('sentences'=>$sentences_positive,'type'=>'positive'), TRUE); ?>
 		</div>
 	<?php endif; ?>
 	<?php if(count($sentences_neutral) > 0) : ?>
-		<p class="h1">Frasi neutrali:</p>
 		<div class="col-sm col-md">
+			<p class="h3">Frasi neutrali:</p>
 			<?php echo $this->load->view('analyzer/sentencesTable', array('sentences'=>$sentences_neutral,'type'=>'neutral'), TRUE); ?>
 		</div>
 	<?php endif; ?>
 	<?php if(count($sentences_negative) > 0) : ?>
-		<p class="h1">Frasi negative:</p>
 		<div class="col-sm col-md">
+			<p class="h3">Frasi negative:</p>
 			<?php echo $this->load->view('analyzer/sentencesTable', array('sentences'=>$sentences_negative,'type'=>'negative'), TRUE); ?>
 		</div>
 	<?php endif; ?>
