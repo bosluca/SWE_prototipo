29918a86f4a1e023c7b9e5a54177de46c4d1b2be
giglium <fre095@gmail.com>
2018-03-05 20:51:28 +0100

    type function

--- application/helpers/google_natural_language_helper.php
+++ application/helpers/google_natural_language_helper.php
@@ -43,8 +43,8 @@ function loadDefaultConfig($language)
 }
 
 /**
- * @param $report result (as array) from analyzeText
- * @param $type type of sentence (positive,neutral,negative)
+ * @param array $report result (as array) from analyzeText
+ * @param string $type type of sentence (positive,neutral,negative)
  * @return array of sentences (positive or negative) and array of sentimen result
  */
 function get_sentences($report = array(), $type = 'positive')
@@ -56,23 +56,9 @@ function get_sentences($report = array(), $type = 'positive')
         foreach($report['sentences'] as $sentence){
             $score = $sentence['sentiment']['score'];
 
-            if($type == 'positive'){
-                if($score > 0.25){
+            if($type == getSimpleType($score)){
                     $sentences[] = $sentence['text']['content'];
                     $scores[]    = $score;
-                }
-            }
-            else if($type == 'negative'){
-                if($score < -0.25){
-                    $sentences[] = $sentence['text']['content'];
-                    $scores[]    = $score;
-                }
-            }
-            else {  // neutral
-                if($score >= -0.25 && $score <= 0.25){
-                    $sentences[] = $sentence['text']['content'];
-                    $scores[]    = $score;
-                }
             }
         }
     }
@@ -83,4 +69,36 @@ function get_sentences($report = array(), $type = 'positive')
     );
 
     return $result;
+}
+
+/**
+ * @param $score integer the score return from the analysis
+ * @return string the type based on the score ( positive, negative or neutral )
+ */
+function getSimpleType($score){
+    if($score > 0.25)
+        return 'positive';
+    if($score < -0.25)
+        return 'negative';
+    return 'neutral';
+}
+
+/**
+ * @param $score integer the score returned from the analysis
+ * @param $magnitude integer the magnitude returned from the analysis
+ * @return string more precise result based on the score and the magnitude ( positive, negative or neutral )
+ */
+function getStrictType($score, $magnitude){
+    if( $score > 0.8 && $magnitude > 3.0 )
+        return 'clearly positive';
+    if($score > 0.25)
+        return 'positive';
+    if($score < -0.6 && $magnitude > 4.0 )
+        return 'clearly negative';
+    if($score < -0.25)
+        return 'negative';
+    if( $score >= -0.25 && $score <= 0.25 && $magnitude === 0 )
+        return 'neutral';
+    if( $score >= -0.25 && $score <= 0.25 && $magnitude > 0 )
+        return 'mixed';
 }
