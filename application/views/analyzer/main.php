b1fb623dd01d1ba87d24f424f5c85ed0b0338f32
Matteo Ceradini <matteo.ceradini@gmail.com>
2018-03-05 18:39:38 +0100

    fix

--- application/views/analyzer/main.php
+++ application/views/analyzer/main.php
@@ -1,19 +1,19 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 <div class="row">
-	<?php if(count($sentences_positive) > 0 || true) : ?>
+	<?php if(count($sentences_positive['sentences']) > 0) : ?>
 		<div class="col-sm col-md">
 			<p class="h3">Frasi positive:</p>
 			<?php echo $this->load->view('analyzer/sentencesTable', array('sentences'=>$sentences_positive,'type'=>'positive'), TRUE); ?>
 		</div>
 	<?php endif; ?>
-	<?php if(count($sentences_neutral) > 0) : ?>
+	<?php if(count($sentences_neutral['sentences']) > 0) : ?>
 		<div class="col-sm col-md">
 			<p class="h3">Frasi neutrali:</p>
 			<?php echo $this->load->view('analyzer/sentencesTable', array('sentences'=>$sentences_neutral,'type'=>'neutral'), TRUE); ?>
 		</div>
 	<?php endif; ?>
-	<?php if(count($sentences_negative) > 0) : ?>
+	<?php if(count($sentences_negative['sentences']) > 0) : ?>
 		<div class="col-sm col-md">
 			<p class="h3">Frasi negative:</p>
 			<?php echo $this->load->view('analyzer/sentencesTable', array('sentences'=>$sentences_negative,'type'=>'negative'), TRUE); ?>
