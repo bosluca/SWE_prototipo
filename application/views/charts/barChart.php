4cef1348edbfc201859a1d082eebb1f7dfc6d4d9
migliorinfa <fre095@gmail.com>
2018-03-06 10:04:05 +0100

    Add comment to function

--- application/views/charts/barChart.php
+++ application/views/charts/barChart.php
@@ -1,9 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 /**
 * @param string $class the css class to give at the chart
 * @param string $labels a string with all the label names, divided by ,
 * @param string $series a string with the series of data, divided by ,
 */
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
 
 echo '<div class="'. $class .'" align="center"></div>';
 
--- application/views/charts/thresholdChart.php
+++ application/views/charts/thresholdChart.php
@@ -1,8 +1,8 @@
-/**
-* @param string $class the css class to give at the chart
-* @param string $series a string with the series of data, divided by ,
-*/
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @param string $class the css class to give at the chart
+ * @param string $series a string with the series of data, divided by ,
+ */
 
 echo '<div class="'. $class .'" align="center"></div>';
 
