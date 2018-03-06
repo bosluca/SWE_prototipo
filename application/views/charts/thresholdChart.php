a601fc2080cac0c6880c9c4d96404ab54cf9295c
giglium <fre095@gmail.com>
2018-03-06 03:27:27 +0100

    Fix speech ongoing chart

--- application/views/charts/thresholdChart.php
+++ application/views/charts/thresholdChart.php
@@ -2,4 +2,4 @@
 
 echo '<div class="'. $class .'" align="center"></div>';
 
-echo '<script>new Chartist.Line(\'.ct-chart\', {series: [['. $series .']]}, {showArea: true,axisY: {onlyInteger: true},plugins: [Chartist.plugins.ctThreshold({threshold: 0})]});</script>';
+echo '<script>new Chartist.Line(\'.' . $class . '\', {series: [['. $series .']]}, {showArea: true,axisY: {onlyInteger: true},plugins: [Chartist.plugins.ctThreshold({threshold: 0})]});</script>';
